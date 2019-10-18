bb2c3e2173868587c5cb653795ff1638cf5c1b94
Student <student@swps.edu.pl>
2019-10-18 20:09:15 +0200

    doad

---
+++ godot do gry/KinematicBody2D.gd
@@ -0,0 +1,21 @@
+extends KinematicBody2D
+var wektorr =  Vector2(0, 0)
+var roacja = 0
+
+func _physics_process(delta):
+	if Input.is_action_pressed('ui_up'):
+	
+	if Input.is_action_pressed('ui_right'):
+	pass
+	if Input.is_action_pressed('ui_left'):
+	pass
+	if Input.is_action_pressed('ui_down'):
+	pass
+
+
+
+
+
+func _ready():
+	pass 
+      
